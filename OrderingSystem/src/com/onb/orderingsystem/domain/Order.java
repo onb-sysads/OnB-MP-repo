b441471f286529149d3a648c7c65db104569d1ad
Cyril Pangilinan <cyril@cyril.orangeandbronze.local>
2011-05-06 16:37:02 +0800

    Added Enumerators, additional test cases

--- OrderingSystem/src/com/onb/orderingsystem/domain/Order.java
+++ OrderingSystem/src/com/onb/orderingsystem/domain/Order.java
@@ -2,15 +2,18 @@ package com.onb.orderingsystem.domain;
 
 import java.math.BigDecimal;
 import java.util.List;
+import com.onb.orderingsystem.utils.Enumerators.*;
 
 public class Order {
 	private int orderID;
 	private List<OrderItem> orderList;
+	private OrderStatus orderStatus = OrderStatus.UNPAID;
 	
-	public Order(int orderID, List<OrderItem> orderList) {
+	public Order(int orderID, List<OrderItem> orderList, OrderStatus orderStatus) {
 		super();
 		this.orderID = orderID;
 		this.orderList = orderList;
+		this.orderStatus = orderStatus;
 	}
 
 	public Order() {
@@ -32,8 +35,16 @@ public class Order {
 	public void setOrderList(List<OrderItem> orderList) {
 		this.orderList = orderList;
 	}
+	
+	public OrderStatus getOrderStatus() {
+		return orderStatus;
+	}
 
-	public BigDecimal computeOrderTotalPrice() {
+	public final void setOrderStatus(OrderStatus orderStatus) {
+		this.orderStatus = orderStatus;
+	}
+
+	public final BigDecimal computeOrderTotalPrice() {
 		BigDecimal totalOrderPrice = new BigDecimal(0.0);
 		for(OrderItem orderItem : orderList){
 			totalOrderPrice = totalOrderPrice.add(orderItem.computeTotalPrice());
